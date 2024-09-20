bd7b6385d6fce1433af79e3c515383f7c030e358
ferni <juanlopezfernandes@gmail.com>
2024-09-20 22:14:11 +0200

    Grid now fills the window

--- main.c
+++ main.c
@@ -1,31 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <raylib.h>
 
-int main(int argc, char* argv[]){
-
-    int gridWidth = 8, gridHeight = 8;
-    const int rectW = 20, rectH = 20;
-
-    if(argc>1){
-        gridWidth = atoi(argv[1]);
-        if(argc>2){
-            gridHeight = atoi(argv[2]);
-        }
-    }
-
-    const int width = 800, height = 600;
+Rectangle* init_grid( const int gridWidth, const int gridHeight, const int rectW, const int rectH){
 
     Rectangle* grid = malloc(sizeof(Rectangle)*gridWidth*gridHeight);
     Rectangle tmp;
 
-    int nextX = 50, nextY = 50;
+    int nextX = 0, nextY = 0;
 
     for(int i = 0; i<gridWidth; i++){
-
-        nextX = 50;
+        nextX = 0;
         for(int j = 0; j<gridHeight; j++){
-            tmp.x = nextX + rectW;
+            tmp.x = nextX;
             tmp.y = nextY;
             tmp.width = rectW;
             tmp.height = rectH;
@@ -34,25 +22,59 @@ int main(int argc, char* argv[]){
 
             nextX += rectW;
         }
-        
         nextY += rectH;
     }
 
-    InitWindow(width, height, "Test");
+    return grid;
+}
 
+int main(int argc, char* argv[]){
+
+    /*window and grid parameters*/
+    const int width = 800, height = 600;
+    int gridWidth = 8, gridHeight = 8;
+
+    /*initialize the window and set it as resizable*/
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(width, height, "Test");
+    
+    /*get grid dimensions from arguments (if provided)*/
+    if(argc>1){
+        gridWidth = atoi(argv[1]);
+        if(argc>2){
+            gridHeight = atoi(argv[2]);
+        }
+    }
+
+    /*set rectangle dimensions based on window size and grid dimensions, grid must fill all the window*/
+    const int rectW = width/gridWidth; 
+    const int rectH = height/gridHeight;
+
+    Rectangle* grid = NULL;
+    
+    /*fill the grid with rectangles*/
+    grid = init_grid(gridWidth, gridHeight, rectW, rectH);
+
+    /*main loop*/
     while(!WindowShouldClose()){
+
+        /*if the window is not minimized draw the grid, otherwise do nothing*/
         if(!IsWindowMinimized()){
             BeginDrawing();
+            ClearBackground(WHITE);
             
             for(int i = 0; i<gridWidth*gridHeight; i++){
                 DrawRectangleLines(grid[i].x, grid[i].y, grid[i].width, grid[i].height, BLACK);
             }
 
-            ClearBackground(WHITE);
             EndDrawing();
+        }
+        else{
+            sleep(1);
         }
     }
 
+    //free resources for grid
     free(grid);
 
     return 0;
